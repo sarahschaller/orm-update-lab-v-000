a3f3ab5424300cb7ed82ab05de29cd3fe1a0e8e0
Sarah Schaller <sarcsch@aol.com>
2017-09-07 19:22:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    name = row[0]
-    name = 
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
 
