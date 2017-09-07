025b95d7c377e09e3140027e0de26a4f2e6b5b06
Sarah Schaller <sarcsch@aol.com>
2017-09-07 18:51:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,12 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id=nil)
-    @name = name 
-    @grade = grade 
-    @id = id 
+    @name = name
+    @grade = grade
+    @id = id
   end
 
 end
